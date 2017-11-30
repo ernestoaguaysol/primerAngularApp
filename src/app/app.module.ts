c962c4620be73de0f62f09f8395f7cddd22a94f3
Ernesto <ernestoaguaysol@gmail.com>
2017-11-29 21:45:19 -0300

    header and footer whit bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './common/header/header.component';
+import { BodyComponent } from './common/body/body.component';
+import { FooterComponent } from './common/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
